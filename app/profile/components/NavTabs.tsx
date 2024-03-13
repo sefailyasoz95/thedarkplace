cc8f6917143c98c0e8d49d5bda15b3d499e506b4
Sefa İlyas Öz <sefailyas1455@gmail.com>
2024-03-13 03:44:02 +0300

    likes tab removed

--- app/profile/components/NavTabs.tsx
+++ app/profile/components/NavTabs.tsx
@@ -4,7 +4,7 @@ import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
-import { BsHeartFill, BsPerson, BsNewspaper } from "react-icons/bs";
+import { BsPerson, BsNewspaper } from "react-icons/bs";
 
 const NavTabs = () => {
 	const pathname = usePathname();
@@ -19,11 +19,6 @@ const NavTabs = () => {
 			text: "Posts",
 			Icon: BsNewspaper,
 		},
-		{
-			href: "/profile/likes",
-			text: "Likes",
-			Icon: BsHeartFill,
-		},
 	];
 	return (
 		<div className='flex items-center gap-5 border-b pb-2 w-full '>
@@ -31,10 +26,10 @@ const NavTabs = () => {
 				<Link
 					href={href}
 					key={index}
-					className={cn("flex items-center border w-full justify-center transition-all ", {
-						"text-red-500 border-red-600 shadow-lg shadow-gray-800": pathname === href,
+					className={cn("flex items-center border rounded-lg w-full justify-center transition-all ", {
+						"text-red-500 border-red-600": pathname === href,
 					})}>
-					<Button variant={"outline"} className='font-semibold gap-2 w-full text-lg'>
+					<Button variant={"outline"} className='font-semibold rounded-lg gap-2 w-full text-lg'>
 						<Icon />
 						{text}
 					</Button>
